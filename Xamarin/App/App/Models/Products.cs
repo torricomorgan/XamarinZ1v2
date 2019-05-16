46870f622b58592e9db99d9fa0f6c5f6881c61f6
Matias Derek Torrico Morgan <matias_dtm@hotmail.com>
2019-05-16 19:49:46 -0400

    COmmit response y token

---
+++ Xamarin/App/App/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Models
+{
+    public class Product
+
+    {
+
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
---
+++ Xamarin/App/App/Models/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Models
+{
+    public class Response
+
+    {
+
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+
+    }
+}
